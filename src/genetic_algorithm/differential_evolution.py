4341204b0c98b09afa252192fc9f03148149aa58
Mark Feaver <markfeaver@weka4.resnet.scms.waikato.ac.nz>
2013-08-14 17:10:28 +1200

    Added selector function. Tidied up methods, etc

--- src/genetic_algorithm/differential_evolution.py
+++ src/genetic_algorithm/differential_evolution.py
@@ -6,44 +6,48 @@ Created on Aug 11, 2013
 @summary: A Python implementation of Differential Evolution, using the DE/rand/1/bin scheme
 '''
 
-import objective_function.SchwefelFunction
 from objective_function.SchwefelFunction import schwefel_function
+from selection_function.RandomSelector import rand_selector
 
 DEF_POP_SIZE=-1
 DEF_CR_RATE=0.4
 DEF_SCALE_FACTOR=0.4
+DEF_SELECTOR=rand_selector
+DEF_NUM_DIFF_VECTORS=1
+DEF_CR_SCHEME="bin"
+DEF_OBJ_FUNCT=schwefel_function
 
 class DifferentialEvolver:
+    _population = []
+    _num_dimensions = 0
     _pop_size = DEF_POP_SIZE
     _cr_rate = DEF_CR_RATE
     _scale_factor = DEF_SCALE_FACTOR
-    _population = []
+    _selector = DEF_SELECTOR
+    _num_diff_vectors = DEF_NUM_DIFF_VECTORS
+    _cr_scheme = DEF_CR_SCHEME
+    _obj_function = DEF_OBJ_FUNCT
+    _scores = []
     
     def init_population(self):
-        global population
-        """ Intialise the population"""
+        """ Initialise the population. """
+        
         print "Initialising Population..."
         
-    def calc_fitness(self, obj_function):
-        """ Calculates the fitness of the current population , using the objective function
+    def calc_fitness(self):
+        """ Calculate the fitness of the current population, using the objective function. """
         
-        Keyword arguments:
-        obj_function -- a function that takes a vector, and returns a fitness rating
-        """
+        for x in xrange(0, self._pop_size):
+            curr = self._population[x]
+            self._scores[x] = self._obj_function(curr)
     
-    def evolve(self, select_type="rand", num_diff_vectors=1, crossover_scheme="bin"):
-        """ Evolve the population using Differential Evolution.
-        
-        Keyword arguments:
-        select_type -- the base-vector selection method, either 'rand' or 'best' (default 'rand')
-        num_diff_vectors -- the number of difference vectors to use (default '1')
-        crossover_scheme -- the crossover scheme to use, either 'bin' for experiments based on independent binomial experiments, or 'exp' for exponential crossover (default 'bin') 
-        """
+    def evolve(self):
+        """ Evolve the population using Differential Evolution. """
         print "Evolving Population..."
         
-        self.calc_fitness(schwefel_function)
+        self.calc_fitness()
         
-    def start(self, num_dimensions, pop_size=DEF_POP_SIZE, crossover_rate=DEF_POP_SIZE, scaling_factor=DEF_SCALE_FACTOR):
+    def start(self, num_dimensions, pop_size=DEF_POP_SIZE, crossover_rate=DEF_POP_SIZE, scaling_factor=DEF_SCALE_FACTOR, selector=DEF_SELECTOR, num_diff_vectors=DEF_NUM_DIFF_VECTORS, crossover_scheme=DEF_CR_SCHEME, obj_function=DEF_OBJ_FUNCT):
         """ Start the DE process
         
         Keyword arguments:
@@ -51,10 +55,22 @@ class DifferentialEvolver:
         pop_size -- the number in the population (default '10 * num_dimensions')
         crossover_rate -- the chance of crossover (default '0.4')
         scaling_factor -- the scaling factor (default '0.4')
+        selector -- the base-vector selection method, either 'rand' or 'best' (default 'rand')
+        num_diff_vectors -- the number of difference vectors to use (default '1')
+        crossover_scheme -- the crossover scheme to use, either 'bin' for experiments based on independent binomial experiments, or 'exp' for exponential crossover (default 'bin')
+        obj_function -- a function that takes a vector, and returns a fitness rating (default 'schwefel_function')
         """
+        
+        self._num_dimensions = num_dimensions
         self._pop_size =  num_dimensions * 10 if (pop_size == DEF_POP_SIZE) else pop_size
         self._cr_rate = crossover_rate
         self._scale_factor = scaling_factor
+        self._selector = selector
+        self._num_diff_vectors = num_diff_vectors
+        self._cr_scheme = crossover_scheme
+        self._obj_function = obj_function
+        
+        self._scores = []
         
         self.init_population()
         self.evolve()
---
+++ src/selection_function/RandomSelector.py
@@ -0,0 +1,11 @@
+'''
+Created on Aug 14, 2013
+
+@author: markfeaver
+'''
+
+import random
+
+def rand_selector(population):
+    """ Select a random individual from the population """
+    return random.choice(population)
