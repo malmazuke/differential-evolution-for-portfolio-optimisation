62ca45c646cb70bb59097fe9870f4eea228418e1
Mark Feaver <markfeaver@weka4.resnet.scms.waikato.ac.nz>
2013-08-14 16:28:53 +1200

    Added an objective function. Continued working on DE

--- src/genetic_algorithm/differential_evolution.py
+++ src/genetic_algorithm/differential_evolution.py
@@ -5,7 +5,11 @@ Created on Aug 11, 2013
 @contact: mark.feaver@gmail.com
 @summary: A Python implementation of Differential Evolution, using the DE/rand/1/bin scheme
 '''
-DEF_POP_SIZE=50
+
+import objective_function.SchwefelFunction
+from objective_function.SchwefelFunction import schwefel_function
+
+DEF_POP_SIZE=-1
 DEF_CR_RATE=0.4
 DEF_SCALE_FACTOR=0.4
 
@@ -20,8 +24,12 @@ class DifferentialEvolver:
         """ Intialise the population"""
         print "Initialising Population..."
         
-    def calc_fitness(self):
-        """ Calculates the fitness of the current population """
+    def calc_fitness(self, obj_function):
+        """ Calculates the fitness of the current population , using the objective function
+        
+        Keyword arguments:
+        obj_function -- a function that takes a vector, and returns a fitness rating
+        """
     
     def evolve(self, select_type="rand", num_diff_vectors=1, crossover_scheme="bin"):
         """ Evolve the population using Differential Evolution.
@@ -33,8 +41,18 @@ class DifferentialEvolver:
         """
         print "Evolving Population..."
         
-    def start(self, pop_size=DEF_POP_SIZE, crossover_rate=DEF_POP_SIZE, scaling_factor=DEF_SCALE_FACTOR):
-        self._pop_size = pop_size
+        self.calc_fitness(schwefel_function)
+        
+    def start(self, num_dimensions, pop_size=DEF_POP_SIZE, crossover_rate=DEF_POP_SIZE, scaling_factor=DEF_SCALE_FACTOR):
+        """ Start the DE process
+        
+        Keyword arguments:
+        num_dimensions -- the number of dimensions in each member of the population
+        pop_size -- the number in the population (default '10 * num_dimensions')
+        crossover_rate -- the chance of crossover (default '0.4')
+        scaling_factor -- the scaling factor (default '0.4')
+        """
+        self._pop_size =  num_dimensions * 10 if (pop_size == DEF_POP_SIZE) else pop_size
         self._cr_rate = crossover_rate
         self._scale_factor = scaling_factor
         
