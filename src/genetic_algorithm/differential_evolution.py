aa28f47aebc7cb4961011213e3ecd61d1049b1d3
Mark Feaver <markfeaver@weka4.resnet.scms.waikato.ac.nz>
2013-08-14 13:51:48 +1200

    Changed to more of a class-like structure

--- src/genetic_algorithm/differential_evolution.py
+++ src/genetic_algorithm/differential_evolution.py
@@ -5,27 +5,42 @@ Created on Aug 11, 2013
 @contact: mark.feaver@gmail.com
 @summary: A Python implementation of Differential Evolution, using the DE/rand/1/bin scheme
 '''
-population = []
+DEF_POP_SIZE=50
+DEF_CR_RATE=0.4
+DEF_SCALE_FACTOR=0.4
 
-def init_population():
-    global population
-    """ Intialise the population"""
-    print "Initialising Population..."
-
-def evolve(select_type="rand", num_diff_vectors=1, crossover_scheme="bin"):
-    """ Evolve the population using Differential Evolution.
+class DifferentialEvolver:
+    _pop_size = DEF_POP_SIZE
+    _cr_rate = DEF_CR_RATE
+    _scale_factor = DEF_SCALE_FACTOR
+    _population = []
     
-    Keyword arguments:
-    select_type -- the base-vector selection method, either 'rand' or 'best' (default 'rand')
-    num_diff_vectors -- the number of difference vectors to use (default '1')
-    crossover_scheme -- the crossover scheme to use, either 'bin' for experiments based on independent binomial experiments, or 'exp' for exponential crossover (default 'bin') 
-    """
-    print "Evolving Population..."
+    def init_population(self):
+        global population
+        """ Intialise the population"""
+        print "Initialising Population..."
+        
+    def calc_fitness(self):
+        """ Calculates the fitness of the current population """
     
-def main():
-    print "Hello DE!"
-    init_population()
-    evolve()
+    def evolve(self, select_type="rand", num_diff_vectors=1, crossover_scheme="bin"):
+        """ Evolve the population using Differential Evolution.
+        
+        Keyword arguments:
+        select_type -- the base-vector selection method, either 'rand' or 'best' (default 'rand')
+        num_diff_vectors -- the number of difference vectors to use (default '1')
+        crossover_scheme -- the crossover scheme to use, either 'bin' for experiments based on independent binomial experiments, or 'exp' for exponential crossover (default 'bin') 
+        """
+        print "Evolving Population..."
+        
+    def start(self, pop_size=DEF_POP_SIZE, crossover_rate=DEF_POP_SIZE, scaling_factor=DEF_SCALE_FACTOR):
+        self._pop_size = pop_size
+        self._cr_rate = crossover_rate
+        self._scale_factor = scaling_factor
+        
+        self.init_population()
+        self.evolve()
     
 if __name__ == '__main__':
-    main()
+    evolver = DifferentialEvolver()
+    evolver.start()
