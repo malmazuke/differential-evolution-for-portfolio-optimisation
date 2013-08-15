a452b13271e716621d5b59b48e2a15957b80d31a
malmazuke <mark.feaver@gmail.com>
2013-08-15 15:18:51 +1200

    Tidied up reading/writing code, etc

--- src/financial_functions/financial_functions.py
+++ src/financial_functions/financial_functions.py
@@ -3,96 +3,110 @@ Created on Aug 15, 2013
 
 @author: markfeaver
 '''
+class Component:
+    """ A component/company in the Dow Jones """
+    
+    _name = ""
+    _prices = []
+    _returns = []
+    _av_daily_return = 0.0
+    
+    def __init__(self, name):
+        self._name = name
+        self._prices = []
+        self._returns = []
+    
+    def add_price(self, price):
+        """ Add the price """
+        self._prices.append(price)
+        
+    def calc_daily_returns(self):
+        self._returns = []
+        for x in xrange(1, len(self._prices)):
+            price_today = self._prices[x]
+            price_yesterday = self._prices[x-1]
+            daily_return = price_today/price_yesterday - 1
+            self._returns.append(daily_return)
+        return self._returns
+    
+    def calc_av_daily_return(self):
+        """ Calculate the average daily return, returning a float. Assumes that the daily returns have already been calculated"""
+        av_return = 0.0
+        total_ret = sum(self._returns)
+        num_averages = len(self._returns)
+        
+        if num_averages > 0:
+            av_return = total_ret/float(num_averages)
+        
+        self._av_daily_return = av_return
+        return av_return
+    
+    def get_av_daily_return(self):
+        return self._av_daily_return
+    
+    def get_name(self):
+        return self._name
+    
+    def __str__(self):
+        out = "(" + self._name
+        if self._av_daily_return != 0.0:
+            out += ", %.4f%%" % self._av_daily_return
+        out += ")" 
+        return out
+    
+    def __repr__(self):
+        return self.__str__()
 
-def average_daily_return(in_csv_file, out_csv_file=""):
-    """ Calculate the Average Daily Return for given year's worth of data, for all companies. Assumes more than 1 day of data.
-     
-    Keyword arguments:
-    csv_file -- a csv file of values from the DowJonesIndustrialAverage, where the first row are the company names, 
-        the first column are the dates (days), the second column is the DJIA, and all other rows are the prices for individual companies      
-    """
-    
-    # These all include the DJ index itself
-    names = []
-    av_daily_returns = []
-    prices_yesterday = []
-    year = ""
-    
-    num_averages = 0
-    
+def read_data(in_csv_file):
+    components = []
     f = open(in_csv_file, 'r')
-
+    
     # Read in the header names
     line = f.readline()
     parts = line.strip().split(',')
     for x in xrange(1, len(parts)):
-        names.append(parts[x]) # Skipping the first column, which contains dates
+        components.append(Component(parts[x])) # Skipping the first column, which contains dates
         
-    # Read in first day
-    line = f.readline()
-    parts = line.strip().split(',')
-    for x in xrange(0, len(parts)):
-        if x == 0:
-            date = parts[x].split('-')
-            year = date[0] + '"'
-            continue
-        av_daily_returns.append(0.0)
-        prices_yesterday.append(float(parts[x]))
-    
-    # Read in the rest of the data, updating as we go
+    # Read in the rest of the data
     for line in f.xreadlines():
         parts = line.strip().split(',')
         for x in xrange(1, len(parts)):
-            # Calculate the return
-            price_today = float(parts[x])
-            price_yesterday = prices_yesterday[x-1]
-            av_daily_returns[x-1] += price_today/price_yesterday - 1
-            # Update yesterdays prices
-            prices_yesterday[x-1] = price_today
-        num_averages += 1
-    
-    out_names = ""
-    # Write the names out (not the most efficient. Oh well)
-    for x in xrange(len(names) + 1):
-        if x == 0:
-            out_names += '"",'
-            continue
-        elif x == len(names):
-            out_names += names[x-1]
-        else:
-            out_names += names[x-1] + ','
-        
-    out_returns = ""
-    # For each index, calculate the average daily return for the entire time period
-    for x in xrange(len(av_daily_returns) + 1):
-        # If it's the first column, write the year
-        if x == 0:
-            out_returns += year + ','
-            continue
-        
-        daily_return = "%.2f%%" % (av_daily_returns[x-1]/float(num_averages)*100)
-        if x == len(names):
-            out_returns += daily_return
-        else:
-            out_returns += daily_return + ','
+            price = float(parts[x])
+            components[x-1].add_price(price)
     
     f.close()
+    return components
     
-    # If we have an output file, write to this
-    if out_csv_file is not "":
-        f_out = open(out_csv_file, 'w')
-        f_out.write(out_names + "\n")
-        f_out.write(out_returns)
-        f_out.close()
-    
-    # Print to std out
-    print(out_names)
-    print(out_returns)
-
 if __name__ == '__main__':
+    components = read_data("../../data/dji.2011.csv")
+    
+    for comp in components:
+        comp.calc_daily_returns()
+        comp.calc_av_daily_return()
+        
     print "Average daily returns 2011:"
-    average_daily_return("../../data/dji.2011.csv", "../../data/av_daily_returns_2011.csv")
-    print "Average daily returns 2012:" 
-    average_daily_return("../../data/dji.2012.csv", "../../data/av_daily_returns_2012.csv")
-    print "Volatility 2011:"
+    # Very inefficient way of outputting data
+    out_names = '"",'
+    out_returns = '"2011",'
+    for x in xrange(len(components)):
+        comp = components[x]
+        if x == len(components):
+            out_names += "%s" % comp.get_name()
+            out_returns += "%.2f%%" % (comp.get_av_daily_return() * 100.0)
+        else:
+            out_names += "%s," % comp.get_name()
+            out_returns += "%.2f%%," % (comp.get_av_daily_return() * 100.0)
+    
+    print out_names
+    print out_returns
+    
+    fw = open("../../data/av_daily_returns_2011.csv", 'w')
+    fw.write(out_names + "\n")
+    fw.write(out_returns)
+    fw.close()
+#     print "Average daily returns 2011:"
+#     average_daily_return("../../data/dji.2011.csv", "../../data/av_daily_returns_2011.csv")
+#     print "Average daily returns 2012:" 
+#     average_daily_return("../../data/dji.2012.csv", "../../data/av_daily_returns_2012.csv")
+#     print "Volatility 2011:"
     
